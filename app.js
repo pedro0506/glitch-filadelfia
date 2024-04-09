6da290e22de0e2c863b10af394a9693259f2909f
pedro0506 <p.souzaunip@gmail.com>
2024-04-09 14:24:47 -0300

    inclusao de novos menus

--- app.js
+++ app.js
@@ -65,57 +65,99 @@ const handleWebhook = (req, res) => {
           if (timeDifference < 300000) {
             // Verificar se a mensagem contém "Menu" para enviar o template "menu"
             if (message.text.body.includes('Menu')) {
-              // axios({
-              //   method: "POST",
-              //   url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
-              //   headers: {
-              //     Authorization: `Bearer ${token}`,
-              //   },
-              //   data: {
-              //     "messaging_product": "whatsapp",
-              //     "recipient_type": "individual",
-              //     "to": message.from,
-              //     "type": "template",
-              //     "template": {
-              //       "name": "menu",
-              //       "language": {
-              //         "code": "pt_BR"
-              //       }
-              //     }
-              //   },
-              // }).catch(error => {
-              //   console.error("Erro ao enviar mensagem:", error);
-              // });
+              axios({
+                method: "POST",
+                url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+                data: {
+                  "messaging_product": "whatsapp",
+                  "recipient_type": "individual",
+                  "to": message.from,
+                  "type": "template",
+                  "template": {
+                    "name": "menu",
+                    "language": {
+                      "code": "pt_BR"
+                    }
+                  }
+                },
+              }).catch(error => {
+                console.error("Erro ao enviar mensagem:", error);
+              });
+            }
+
+            if (message.text.body == '4') {
+              axios({
+                method: "POST",
+                url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+                data: {
+                  "messaging_product": "whatsapp",
+                  "recipient_type": "individual",
+                  "to": message.from,
+                  "text": {
+                    "preview_url": true,
+                    "body": "Ainda nenhum evento disponível. \n\nInformações sobre eventos enviar mensagem para o número +55 33 9961-0892."
+                  }
+                },
+              }).catch(error => {
+                console.error("Erro ao enviar mensagem:", error);
+              });
+            }
+
+            if (message.text.body == '5') {
+              axios({
+                method: "POST",
+                url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+                data: {
+                  "messaging_product": "whatsapp",
+                  "recipient_type": "individual",
+                  "to": message.from,
+                  "text": {
+                    "preview_url": true,
+                    "body": "Dúvidas frequentes: \n\na. Aquisição de cota \nb. Venda de Cota \nc.Compra de cota terceiro \nd. Fotografias no clube (como funciona)? \ne. Falar com um Atendente \n\n Informe a opção desejada digitando apenas a letra.\nEx: 'Venda de Cota' digite b "
+                  }
+                },
+              }).catch(error => {
+                console.error("Erro ao enviar mensagem:", error);
+              });
             }
     
             // Verificar se é hora de enviar o template "hello"
             if (
               (!lastHelloSent[message.from] || (currentTime - lastHelloSent[message.from] > 3600000)) // Enviar apenas uma vez por hora
             ) {
-              // axios({
-              //   method: "POST",
-              //   url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
-              //   headers: {
-              //     Authorization: `Bearer ${token}`,
-              //   },
-              //   data: {
-              //     "messaging_product": "whatsapp",
-              //     "recipient_type": "individual",
-              //     "to": message.from,
-              //     "type": "template",
-              //     "template": {
-              //       "name": "hello",
-              //       "language": {
-              //         "code": "pt_BR"
-              //       }
-              //     }
-              //   },
-              // }).then(() => {
-              //   // Registrar o momento em que o template "hello" foi enviado
-              //   lastHelloSent[message.from] = currentTime;
-              // }).catch(error => {
-              //   console.error("Erro ao enviar mensagem:", error);
-              // });
+              axios({
+                method: "POST",
+                url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+                data: {
+                  "messaging_product": "whatsapp",
+                  "recipient_type": "individual",
+                  "to": message.from,
+                  "type": "template",
+                  "template": {
+                    "name": "hello",
+                    "language": {
+                      "code": "pt_BR"
+                    }
+                  }
+                },
+              }).then(() => {
+                // Registrar o momento em que o template "hello" foi enviado
+                lastHelloSent[message.from] = currentTime;
+              }).catch(error => {
+                console.error("Erro ao enviar mensagem:", error);
+              });
             }
           }
         }
